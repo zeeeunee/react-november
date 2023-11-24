f234a14984bf4ae4a7a45ba0b0eaf0624d2aa667
zeeeunee <wldmstv@gmail.com>
2023-11-24 13:10:48 +0900

    커스텀훅을 통한 각 서브페이지 제목의 분리

--- src/components/common/layout/Layout.jsx
+++ src/components/common/layout/Layout.jsx
@@ -4,24 +4,18 @@ import { useSplitText } from '../../../hooks/useSplitText';
 
 export default function Layout({ children, title }) {
 	const refFrame = useRef(null);
+	const refTitle = useRef(null);
 
-	//useEffect안쪽에서 자주쓰일만한 특정 함수를 호출해야됨
-	//use로 시작하는 커스텀훅은 특정함수 안쪽에서 호출불가
-	//해당 hook이 함수를 반환하도록 처리
 	const splitText = useSplitText();
-	console.log(splitText);
 
 	useEffect(() => {
+		splitText(refTitle.current, title);
 		refFrame.current.classList.add('on');
 	}, []);
 
-	//아래처럼 custom hook이 반환한 함수를
-	//hook이나 핸들러함수 내부에서 사용가능
-	splitText('hello');
-
 	return (
 		<main ref={refFrame} className={`Layout ${title}`}>
-			<h1>{title}</h1>
+			<h1 ref={refTitle}>{title}</h1>
 			<div className='bar'></div>
 			{/*Layout컴포넌트로 감싼 컨텐츠 내용이 아래 children위치에 출력됨 */}
 			{children}
--- src/hooks/useSplitText.js
+++ src/hooks/useSplitText.js
@@ -5,7 +5,13 @@
 
 export function useSplitText() {
 	//해당 useSplitText훅은 호출시 아래의 함수를 리턴
-	return (txt) => {
-		console.log(txt);
+	return (ref, txt) => {
+		let tags = '';
+
+		for (let letter of txt) {
+			tags += `<span>${letter}</span>`;
+		}
+		console.log(tags);
+		ref.innerHTML = tags;
 	};
 }
