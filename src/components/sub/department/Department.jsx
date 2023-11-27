555be9e0616aca7c7420fcbd0f31634e4f54bb29
zeeeunee <wldmstv@gmail.com>
2023-11-27 13:18:32 +0900

    문자열 관련 자주쓰는 내장메서드 소개, 첫번재 문자만 자동으로 대문자 변경로직 적용

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -3,8 +3,13 @@ import Layout from '../../common/layout/Layout';
 import './Department.scss';
 
 export default function Department() {
-	const [MemberTit, setMemberTit] = useState('');
+	//const test = 'abcdef';
+	//console.log(test.charAt(0)); //a
+	//console.log(test.slice(1, 3)); //bc
+	//console.log(test.slice(1)); //bcdef
+	//console.log(test.toUpperCase());
 
+	const [MemberTit, setMemberTit] = useState('');
 	const [MemberData, setMemberData] = useState([]);
 	const path = process.env.PUBLIC_URL; //public폴더까지의 경로를 구하는 구문
 
@@ -12,8 +17,8 @@ export default function Department() {
 		fetch(`${path}/DB/department.json`)
 			.then((data) => data.json())
 			.then((json) => {
-				console.log('key', Object.keys(json)[0]);
-				console.log('value', Object.values(json)[0]);
+				console.log('key', Object.keys(json)[0]); //객체를 반복돌며 key값만 배열로 반환
+				console.log('value', Object.values(json)[0]); //객체를 반복돌며 value값만 배열로 반환
 				setMemberTit(Object.keys(json)[0]);
 				setMemberData(Object.values(json)[0]);
 			});
@@ -25,7 +30,7 @@ export default function Department() {
 	return (
 		<Layout title={'Department'}>
 			<section className='memberBox'>
-				<h2>{MemberTit}</h2>
+				<h2>{`${MemberTit.charAt(0).toUpperCase() + MemberTit.slice(1)}`}</h2>
 				{MemberData.map((member, idx) => {
 					return (
 						<article key={member + idx}>
@@ -47,9 +52,21 @@ export default function Department() {
 	3. 위에서 만든 함수를 의존성배열이 비어있는 useEffect문 안쪽에서 호출 (다음번 렌더링 타이밍에 State값 활용가능)
 	4. State에 담겨있는 데이터 배열값을 map으로 반복 돌면서 JSX구문 생성
 
+
 	객체의 property에서 key, value값 반복도는 방법
 	const student={name:'David',age:20}
 	//key반복들면서 배열반환
 	Object.keys(student); ['name','age'];
 	Object.values(student); ['David',20];
+
+
+	
+//문자열 관련 내장 메서드
+	전체문자열.charAt(순서) :전체문자열에서 해당 순서의 문자값만 반환
+	전체문자열.slice(순서1, 순서2) : 전체 문자열에서 해당 순서1부터 순서2위치까지 문자를 잘라서 반환
+	전체문자열.upperCase() : 문자열을 전체 대문자로 반환
+	전체문자열.lowerCase() : 문자열을 전체 소문자로 반환
+	전체문자열.split(구분자) : 전체문자열을 구분자를 기준으로 나눠서 배열로 반환
+	배열.join('구분자') : 각 배열값을 구분자로 이어붙이면서 하나의 문자열로 반환
+
 */
