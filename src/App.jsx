e0db5b9bff6a3846d531ca0ffe37dd47d7a6b210
zeeeunee <wldmstv@gmail.com>
2023-11-29 12:30:27 +0900

    테블릿 반응형처리

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,13 @@ import { Route } from 'react-router-dom';
 import './globalStyles/Variables.scss';
 import './globalStyles/Reset.scss';
 import { useState } from 'react';
+import { useMedia } from './hooks/useMedia';
 
 function App() {
 	const [Dark, setDark] = useState(false);
 
 	return (
-		<div className={Dark ? 'wrap dark' : 'wrap'}>
+		<div className={`wrap ${Dark ? 'dark' : ''} ${useMedia()}`}>
 			<Header Dark={Dark} setDark={setDark} />
 			<Route exact path='/' component={MainWrap} />
 			<Route path='/department' component={Department} />
