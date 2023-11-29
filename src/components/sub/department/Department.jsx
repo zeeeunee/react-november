bada3f173d6e802b9bed3b6943a28eb3b4abb94b
zeeeunee <wldmstv@gmail.com>
2023-11-29 10:29:27 +0900

    json파일 가상돔 출력

--- src/components/sub/department/Department.jsx
+++ src/components/sub/department/Department.jsx
@@ -4,6 +4,9 @@ import './Department.scss';
 import { useCustomText } from '../../../hooks/useText';
 
 export default function Department() {
+	const [HistoryTit, setHistoryTit] = useState('');
+	const [HistoryData, setHistoryData] = useState([]);
+
 	const [MemberTit, setMemberTit] = useState('');
 	const [MemberData, setMemberData] = useState([]);
 	const path = useRef(process.env.PUBLIC_URL); //public폴더까지의 경로를 구하는 구문
@@ -34,12 +37,42 @@ export default function Department() {
 			});
 	};
 
+	const fetchHistory = () => {
+		fetch(`${path.current}/DB/history.json`)
+			.then((data) => data.json())
+			.then((json) => {
+				setHistoryTit(Object.keys(json)[0]);
+				setHistoryData(Object.values(json)[0]);
+			});
+	};
+
 	useEffect(() => {
 		fetchDepartment();
+		fetchHistory();
 	}, []);
 
 	return (
 		<Layout title={'Department'}>
+			<section className='historyBox'>
+				<h2>{combinedTitle(HistoryTit)}</h2>
+				<div className='con'>
+					{/* HistoryData가 반복도는 각각의 데이터 {년도: 배열} */}
+					{HistoryData.map((history, idx) => {
+						return (
+							<article key={history + idx}>
+								{/* 현재 반복돌고 있는 객체의 key값을 뽑아서 h3로 출력 :2016 */}
+								<h3>{Object.keys(history)[0]}</h3>
+								<ul>
+									{/* 현재 반복돌고 있는 객체의 value을 뽑아서 li로 반복출력 [문자열, 문자열]  */}
+									{Object.values(history)[0].map((list, idx) => {
+										return <li key={list + idx}>{list}</li>;
+									})}
+								</ul>
+							</article>
+						);
+					})}
+				</div>
+			</section>
 			<section className='memberBox'>
 				<h2>{combinedTitle(MemberTit)}</h2>
 
