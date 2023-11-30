b5ba3d037bdc60e894b54db513b503e88d30c213
zeeeunee <wldmstv@gmail.com>
2023-11-30 11:00:59 +0900

    state배열값을 반복돌면서 JSX출력하고 문자열을 커스텀훅으로 가공

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,8 +1,11 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
 import { useState, useEffect } from 'react';
+import { useCustomText } from '../../../hooks/useText';
 
 export default function Youtube() {
+	const customText = useCustomText('combined');
+	const shortenText = useCustomText('shorten');
 	const [Vids, setVids] = useState([]);
 
 	//promise then 구문을 async await 변경하기 위한 조건 2가지
@@ -33,5 +36,29 @@ export default function Youtube() {
 		fetchYoutube();
 	}, []);
 
-	return <Layout title={'Youtube'}>Youtube</Layout>;
+	return (
+		<Layout title={'Youtube'}>
+			{Vids.map((data, idx) => {
+				const [date, time] = data.snippet.publishedAt.split('T');
+
+				return (
+					<article key={data.id}>
+						<h2>{shortenText(data.snippet.title, 50)}</h2>
+
+						<div className='txt'>
+							<p>{shortenText(data.snippet.description, 250)}</p>
+							<div className='infoBox'>
+								<span>{customText(date, '.')}</span>
+								<em>{time.split('Z')[0]}</em>
+							</div>
+						</div>
+
+						<div className='pic'>
+							<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+						</div>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
--- src/hooks/useText.js
+++ src/hooks/useText.js
@@ -38,11 +38,11 @@ export function useCustomText(type) {
 	if (type === 'combined') {
 		//regEx (regular expression: 정규표현식) 문자열의 패턴별로 특정 기능 수행식
 		// /정규표현식/
-		return (txt) => {
+		return (txt, spc = ' ') => {
 			const resultText = txt
 				.split(/-|_|\+/) //인수로 들어가는 특수문자가 -,_,+일때는 해당 구분자로 문자를 분리함 (예약어 문자열을 앞에 \붙여서 처리)
 				.map((data) => toUpperText(data))
-				.join(' ');
+				.join(spc);
 			return resultText;
 		};
 	}
