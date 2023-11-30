9d137507578cb61d85a28c6b8611437f1c31e055
zeeeunee <wldmstv@gmail.com>
2023-11-30 13:16:44 +0900

    youtube 상세페이지 및 영상 출력

--- src/components/sub/youtube/Detail.jsx
+++ src/components/sub/youtube/Detail.jsx
@@ -9,19 +9,23 @@ export default function Detail() {
 
 	const fetchSingleData = async () => {
 		const api_key = 'AIzaSyBQ0OBVJR5LwVP7O1wFRSbfMbLCLvWRLnE';
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&Id=${id}`;
+		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&id=${id}`;
 
 		const data = await fetch(baseURL);
 		const json = await data.json();
 		setYoutubeData(json.items[0].snippet);
 	};
-
 	useEffect(() => {
 		fetchSingleData();
 	}, []);
 	return (
 		<Layout title={'Detail'}>
-			<h3></h3>
+			{/* Optional Chaing : 객체명?.property 해당객체에 값이 없을땐 무시하고 값이 있을때만 property접근 */}
+			<div className='videoBox'>
+				<iframe src={`https://www.youtube.com/embed/${YoutubeData?.resourceId.videoId}`} title={YoutubeData?.title}></iframe>
+			</div>
+			<h3>{YoutubeData?.title}</h3>
+			<p>{YoutubeData?.description}</p>
 		</Layout>
 	);
 }
