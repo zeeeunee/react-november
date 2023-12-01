4988a7daa850415c16015a2effe9ef415abd2762
zeeeunee <wldmstv@gmail.com>
2023-12-01 11:35:40 +0900

    flickr data호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 
 export default function Gallery() {
-	return <Layout title={'Gallery'}>Gallery</Layout>;
+	const [Pics, setPics] = useState([]);
+
+	const fetchFlickr = async () => {
+		const num = 500;
+		const flickr_api = '7973628e19035e31ccf3734cc641b14f';
+		const method_interest = 'flickr.interestingness.getList';
+		const baseURL = 'https://www.flickr.com/services/rest/?method=';
+		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+
+		setPics(json.photos.photo);
+	};
+
+	useEffect(() => {
+		fetchFlickr();
+	}, []);
+
+	return (
+		<Layout title={'Gallery'}>
+			{Pics.map((pic, idx) => {
+				return (
+					<article key={pic.id}>
+						<h2>{pic.title}</h2>
+					</article>
+				);
+			})}
+		</Layout>
+	);
 }
