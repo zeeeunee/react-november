cdabf53b3153643e1239d3dae7f657a7025e7156
zeeeunee <wldmstv@gmail.com>
2023-12-04 10:15:39 +0900

    user 아이디로 갤러리 호출

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -8,12 +8,15 @@ export default function Gallery() {
 
 	const fetchFlickr = async () => {
 		const num = 100;
+		const myID = '199697926@N08';
 		const flickr_api = process.env.REACT_APP_YOUTUBE_FLICKR_API;
+		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 		const method_interest = 'flickr.interestingness.getList';
-		const baseURL = 'https://www.flickr.com/services/rest/?method=';
-		const resultURL = `${baseURL}${method_interest}&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1`;
+		const method_user = 'flickr.people.getPhotos';
+		const interestURL = `${baseURL}${method_interest}`;
+		const userURL = `${baseURL}${method_user}&user_id=${myID}`;
 
-		const data = await fetch(resultURL);
+		const data = await fetch(userURL);
 		const json = await data.json();
 
 		setPics(json.photos.photo);
@@ -26,30 +29,30 @@ export default function Gallery() {
 	return (
 		<Layout title={'Gallery'}>
 			<section className='frame'>
-				{/* <Masonry className={'frame'} options={{ transitionDuration: 0.5 }}> */}
-				{Pics.map((pic, idx) => {
-					return (
-						<article key={pic.id}>
-							<div className='pic'>
-								<img
-									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
-									alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-								/>
-							</div>
-							<h2>{pic.title}</h2>
+				<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: 20 }}>
+					{Pics.map((pic, idx) => {
+						return (
+							<article key={pic.id}>
+								<div className='pic'>
+									<img
+										src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+									/>
+								</div>
+								<h2>{pic.title}</h2>
 
-							<div className='profile'>
-								<img
-									src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
-									alt='사용자프로필이미지'
-									onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
-								/>
-								<span>{pic.owner}</span>
-							</div>
-						</article>
-					);
-				})}
-				{/* </Masonry> */}
+								<div className='profile'>
+									<img
+										src={`http://farm${pic.farm}.staticflickr.com/${pic.server}/buddyicons/${pic.owner}.jpg`}
+										alt='사용자프로필이미지'
+										onError={(e) => e.target.setAttribute('src', 'https://www.flickr.com/images/buddyicon.gif')}
+									/>
+									<span>{pic.owner}</span>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
 			</section>
 		</Layout>
 	);
