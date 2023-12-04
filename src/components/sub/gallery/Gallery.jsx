edcd712a86a31833967708c94a9aabc91f753cfb
zeeeunee <wldmstv@gmail.com>
2023-12-04 10:56:22 +0900

    user타입의 갤러리 함수 호출시 추가 인수로 사용자 아이디를 직접 전달하도록 코드 개선

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,33 +1,51 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
+	//1-참조객체에 내 아이디값 등록
+	const myID = useRef('199697926@N08');
 
-	const fetchFlickr = async () => {
+	const fetchFlickr = async (opt) => {
 		const num = 100;
-		const myID = '199697926@N08';
 		const flickr_api = process.env.REACT_APP_YOUTUBE_FLICKR_API;
 		const baseURL = `https://www.flickr.com/services/rest/?&api_key=${flickr_api}&per_page=${num}&format=json&nojsoncallback=1&method=`;
 		const method_interest = 'flickr.interestingness.getList';
 		const method_user = 'flickr.people.getPhotos';
-		const interestURL = `${baseURL}${method_interest}`;
-		const userURL = `${baseURL}${method_user}&user_id=${myID}`;
 
-		const data = await fetch(userURL);
+		const interestURL = `${baseURL}${method_interest}`;
+
+		//3- userURL에는 user_id를 상수값이 아닌 호출시점에 전달된 opt객체의 id로 등록해서 URL생성
+		const userURL = `${baseURL}${method_user}&user_id=${opt.id}`;
+
+		let url = '';
+
+		//4- 만들어진 URL로 데이터요청
+		opt.type === 'user' && (url = userURL);
+		opt.type === 'interest' && (url = interestURL);
+
+		const data = await fetch(url);
 		const json = await data.json();
 
 		setPics(json.photos.photo);
 	};
 
 	useEffect(() => {
-		fetchFlickr();
+		//2-처음 컴포넌트 마운트시 타입을 user로 지정하고 id값으로 내 아이디등록
+		fetchFlickr({ type: 'user', id: myID.current });
+		//fetchFlickr({ type: 'interest' });
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
+			<article className='controls'>
+				<nav className='btnSet'>
+					<button onClick={() => fetchFlickr({ type: 'interest' })}>Interest Gallery</button>
+					<button onClick={() => fetchFlickr({ type: 'user', id: myID.current })}>My Gallery</button>
+				</nav>
+			</article>
 			<section className='frame'>
 				<Masonry className={'frame'} options={{ transitionDuration: '0.5s', gutter: 20 }}>
 					{Pics.map((pic, idx) => {
