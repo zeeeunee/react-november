98d9c0803bdeb83c8fe44605e34a09e34c1c2bd7
zeeeunee <wldmstv@gmail.com>
2023-12-04 12:22:23 +0900

    사용자 아이디 클릭한 상태에서 다시 my gallery 출력이 안되는 문제 수정

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -6,7 +6,8 @@ import Masonry from 'react-masonry-component';
 export default function Gallery() {
 	console.log('re-render');
 	const myID = useRef('199697926@N08'); //1-참조객체에 내 아이디값 등록
-	const isUser = useRef(true);
+
+	const isUser = useRef(myID.current); //isUser의 초기값을 내 아이디 문자값으로 등록
 	const refNav = useRef(null);
 	const [Pics, setPics] = useState([]);
 
@@ -18,20 +19,26 @@ export default function Gallery() {
 
 	const handleInterest = (e) => {
 		if (e.target.classList.contains('on')) return;
-		isUser.current = false;
+		//Interest함수 호출시 isUser값을 빈문자열로 초기화 (false로 인식되는 값)
+		isUser.current = '';
 		activateBtn(e);
 		fetchFlickr({ type: 'interest' });
 	};
 
 	const handleMine = (e) => {
-		if (e.target.classList.contains('on') || isUser.current) return;
+		//콕 찍어서 isUser의 값과 myID값이 동일할때만 함수 중지
+		//마이갤러리 함수 호출시에는 isUser의 문자값이 담겨있다고 하더라도 내 아이디와 똑같지 않으면 핸들러함수를 실행하게 처리
+		//다른 사용자 갤러리를 갔다가 다시 myGallery호출시 이미 다른 사용자 유저 id가 담겨있기 때문에 내 갤러리가 호출되지 않는 문제를 해결하기 위함
+		if (e.target.classList.contains('on') || isUser.current === myID.current) return;
+		isUser.current = myID.current;
 		activateBtn(e);
 		fetchFlickr({ type: 'user', id: myID.current });
 	};
 
 	const handleUser = (e) => {
+		//isUser값이 비어있기만 하면 중지
 		if (isUser.current) return;
-		isUser.current = true;
+		isUser.current = e.target.innerText;
 		activateBtn();
 		fetchFlickr({ type: 'user', id: e.target.innerText });
 	};
