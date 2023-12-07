1d3b3518ce352180c966fa3686b62194cc0700c7
zeeeunee <wldmstv@gmail.com>
2023-12-07 11:34:40 +0900

    날짜 변환 작업 흐름 주석 설명

--- src/components/sub/community/Community.jsx
+++ src/components/sub/community/Community.jsx
@@ -6,6 +6,7 @@ import { TfiWrite } from 'react-icons/tfi';
 import { useCustomText } from '../../../hooks/useText';
 
 export default function Community() {
+	//추후에 가져올 시간값에서 -을 .으로 변경하기 위해서 combined타입의 텍스트변환 함수를 텍스트관력 훅으로부터 활성화
 	const changeText = useCustomText('combined');
 	const getLocalData = () => {
 		const data = localStorage.getItem('post');
@@ -31,7 +32,11 @@ export default function Community() {
 			resetPost();
 			return alert('제목과 본문을 모두 입력하세요.');
 		}
+
+		//기존 시간인스턴스값을 한국시에 맞게 변경
+		//new Date().toLocalString(): 해당 지역의 표준시로 변환 (단점, 원하지 않는 방향으로 가공됨)
 		const korTime = new Date().getTime() + 1000 * 60 * 60 * 9;
+		//한국시로 변환된 시간 객체값을 date키값에 추가로 등록해서 State에 저장
 		setPost([{ title: refTit.current.value, content: refCon.current.value, date: new Date(korTime) }, ...Post]);
 		resetPost();
 	};
@@ -69,7 +74,11 @@ export default function Community() {
 				</div>
 				<div className='showBox'>
 					{Post.map((el, idx) => {
+						//시간값을 getLocalDate함수를 통해서 시간인스턴스객체값을 객체상태로 그대로 JSX안쪽의 {}에 넣을 수 없으므로
+						//변환된 시간 객체값을 다시 강제로 문자화
 						const date = JSON.stringify(el.date);
+						//문자화시킨 값에서 먼저 T기점으로 앞의 시간문자를 찾고 다시 맨앞의 "를 제외한 나머지 문자 반환(년도-월-일)
+						//반환된 문자값을 다시 changeText의 인수로 전달해서 (년도.월.일)로 변환
 						const strDate = changeText(date.split('T')[0].slice(1), '.');
 						console.log(strDate);
 						return (
@@ -77,6 +86,7 @@ export default function Community() {
 								<div className='txt'>
 									<h2>{el.title}</h2>
 									<p>{el.content}</p>
+									{/* 변환된 날자값 최종 출력 */}
 									<span>{strDate}</span>
 								</div>
 								<nav>
