b6b1f43a436699a1769abcdddac1ede846a777d4
zeeeunee <wldmstv@gmail.com>
2023-12-08 13:24:37 +0900

    지도 정보 객체 형태로 분리

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,29 +1,54 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
 
 export default function Contact() {
-	const mapFrame = useRef(null);
-	const { kakao } = window;
-	const mapOption = useRef({ center: new kakao.maps.LatLng(37.511553, 127.022009), level: 3 });
+	const [Index, setIndex] = useState(2);
 
-	const imageSrc = process.env.PUBLIC_URL + `/img/marker1.png`;
-	const imageSize = new kakao.maps.Size(242, 99);
-	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const { kakao } = window;
+	const mapFrame = useRef(null);
+	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
+	const mapInfo = useRef([
+		{
+			title: '삼성역 코엑스',
+			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	]);
+
+	//마커 인스턴스 생성
+	const markerInstance = new kakao.maps.Marker({
+		position: mapInfo.current[Index].latlng,
+		image: new kakao.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
+	});
 
 	useEffect(() => {
-		const mapInstance = new kakao.maps.Map(mapFrame.current, mapOption.current);
-		const markerImageInstance = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-		const posInstance = mapOption.current.center;
-		const markerInstance = new kakao.maps.Marker({
-			position: posInstance,
-			image: markerImageInstance,
+		const mapInstance = new kakao.maps.Map(mapFrame.current, {
+			center: mapInfo.current[Index].latlng,
+			level: 3,
 		});
 		markerInstance.setMap(mapInstance);
 	}, []);
+
 	return (
 		<Layout title={'Contact'}>
-			<article id='map' ref={mapFrame}></article>
+			<article className='mapBox' ref={mapFrame}></article>
 		</Layout>
 	);
 }
