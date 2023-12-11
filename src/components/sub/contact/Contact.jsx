e515718a4eefabd412f32dd10bc06502559d1b04
zeeeunee <wldmstv@gmail.com>
2023-12-11 12:25:09 +0900

    토글버튼으로 지도,로드뷰 전환

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -1,15 +1,15 @@
 import { useEffect, useRef, useState } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Contact.scss';
-
 export default function Contact() {
 	const kakao = useRef(window.kakao);
 	//화면에 출력될 지도정보 배열의 순번이 담길 state
 	const [Index, setIndex] = useState(0);
 	const [Traffic, setTraffic] = useState(false);
+	const [View, setView] = useState(false);
+
 	const mapFrame = useRef(null);
 	const viewFrame = useRef(null);
-
 	const marker = useRef(null);
 	const mapInstance = useRef(null);
 	//지점마다 출력할 정보를 개별적인 객체로 묶어서 배열로 그룹화
@@ -41,9 +41,7 @@ export default function Contact() {
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt),
 	});
-
 	const setCenter = () => mapInstance.current.setCenter(mapInfo.current[Index].latlng);
-
 	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
@@ -53,30 +51,23 @@ export default function Contact() {
 		});
 		marker.current.setMap(mapInstance.current);
 		setTraffic(false);
-
 		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, (panoId) => {
 			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
 		});
-
 		//지도 타입 컨트롤러 추가
 		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
-
 		//지도 줌 컨트롤러 추가
 		mapInstance.current.addControl(new kakao.current.maps.ZoomControl(), kakao.current.maps.ControlPosition.RIGHT);
-
-		//마우스휠 지도 줌기능 비활성
+		//휠에 맵 줌 기능 비활성화
 		mapInstance.current.setZoomable(false);
-
 		window.addEventListener('resize', setCenter);
 		return () => window.removeEventListener('resize', setCenter);
 	}, [Index]);
-
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
-
 	return (
 		<Layout title={'Contact'}>
 			<div className='controlBox'>
@@ -90,15 +81,16 @@ export default function Contact() {
 
 				<nav className='info'>
 					<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic OFF' : 'Traffic ON'}</button>
+					<button onClick={() => setView(!View)}>{View ? 'map' : 'road view'}</button>
 				</nav>
 			</div>
-
-			<article className='mapBox' ref={mapFrame}></article>
-			<article className='viewBox' ref={viewFrame}></article>
+			<section className='tab'>
+				<article className={`mapBox ${View ? '' : 'on'}`} ref={mapFrame}></article>
+				<article className={`viewBox ${View ? 'on' : ''}`} ref={viewFrame}></article>
+			</section>
 		</Layout>
 	);
 }
-
 /*
 1.cdn불러온 window에 불러온 외부 객체값을 가져와서 인스턴스생성
 2.인스턴스값을 참조객체 담는 이유 ( 의존성배열에 불필요하게 등록하지 않기 위해서)
