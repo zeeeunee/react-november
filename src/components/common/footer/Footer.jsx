0d1f7336509fb7fa2fd2935abfba88a0fdbe04f9
zeeeunee <wldmstv@gmail.com>
2023-12-14 12:43:48 +0900

    외부 비동기데이터(서버사이드데이터)를 전역 store에 저장할떄 dispatch와 action객체가 필요한 이유

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,12 +1,17 @@
+import { useSelector } from 'react-redux';
 import './Footer.scss';
 import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
 //npm i react-icons 로 아이콘설치해서 적용하기
 
 export default function Footer() {
+	const { name, position } = useSelector(store => store.memberReducer.members[0]);
 	return (
 		<footer className='Footer'>
 			<h1>Zeeeunee</h1>
 			<p>2023 Zeeeunee &Copy; All Rights Reserved.</p>
+			<h2>
+				{position}:{name}
+			</h2>
 			<ul>
 				<li>
 					<FaFacebook />
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,14 @@
 import { combineReducers } from 'redux';
 
+//dispatch가 필요한 이유
+//원래 데이터는 자체DB이던 외부 API데이터이던 어쨌든 fetching을 통해서 외부데이터를 가져와야됨
+//그래서 위와 같이 reducer안쪽에 초기 데이터를 설정하는 것이 불가능
+
+//dispatch로 외부데이터를 fetching후 전역 state에 담는 순서
+//1. 컴포넌트에서 useEffect로 mount시 fetching함수 호출후 데이터 반환
+//2. 해당데이터를 지역state에 담는 것이 아닌 action객체의 payload에 담아서 dispatch로 reducer에 전달
+//3. 아래 reducer함수 로직에 의해서 fetching된 데이터가 store에 전달되고
+//4. 이후 각 컴포넌트에서 useSelector로 해당 데이터에 자유롭게 접근 가능
 const initMember = {
 	members: [
 		{
