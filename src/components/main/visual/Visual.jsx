e0f19cc016f3f1a79e5893bf22698bdcffbc93c8
zeeeunee <wldmstv@gmail.com>
2023-12-15 11:47:10 +0900

    데이터별 액션타입 객체로 등록

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -9,8 +9,8 @@ export default function Visual() {
 				//Vids의 유무로 에러처리를 할 수 없는 이유
 				//설사 데이터 반환에 실패하더라도 Vids에는 undefined라는 값이 들어가 있기때문에
 				//데이터반환에 실패해서 분기처리 하기 위해서는 err객체에만 있는 message라는 property로 분기처리
-				//Vids뒤에 무조건 optionial chaing처리를 하는 이유는
-				//해당 리액트가 조건문을 읽때 Vids값이 undefined이기 떄문에 undefined에 message, map 프로퍼티 접근 자체가 구문 오류이기 때문에 초기 구문오류 에러를 피하기 위함
+				//Vids뒤에 무조건 optionial chaing(물음표처리)처리를 하는 이유는
+				//해당 리액트가 조건문을 읽을때 Vids값이 undefined이기 떄문에 undefined에 message, map 프로퍼티 접근 자체가 구문 오류이기 때문에 초기 구문오류 에러를 피하기 위함
 				Vids?.message ? (
 					<h1>{Vids?.message}</h1>
 				) : (
---
+++ src/redux/action.js
@@ -0,0 +1,18 @@
+//각 데이터 카테고리별 사용될 액션타입명을 변수처럼 모아놓은 객체
+export const MEMBER = {
+	start: 'MEMBER_START',
+	success: 'MEMBER_SUCCESS',
+	fail: 'MEMBER_FAIL'
+};
+
+export const HISTORY = {
+	start: 'HISTORY_START',
+	success: 'HISTORY_SUCCESS',
+	fail: 'HISTORY_FAIL'
+};
+
+export const YOUTUBE = {
+	start: 'YOUTUBE_START',
+	success: 'YOUTUBE_SUCCESS',
+	fail: 'YOUTUBE_FAIL'
+};
