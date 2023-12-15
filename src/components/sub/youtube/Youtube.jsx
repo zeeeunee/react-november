f0074d8dc8b5d9417ab194f7ea604b6c87688978
zeeeunee <wldmstv@gmail.com>
2023-12-15 11:05:24 +0900

    youtube전역데이터관리

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,45 +1,21 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
-import { useState, useEffect } from 'react';
 import { useCustomText } from '../../../hooks/useText';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useSelector } from 'react-redux';
 
 export default function Youtube() {
+	const Vids = useSelector(store => store.youtubeReducer.youtube);
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('shorten');
-	const [Vids, setVids] = useState([]);
 
 	//promise then 구문을 async await 변경하기 위한 조건 2가지
 	//조건1 - promise반환 함수를 감싸주는 wrapping함수 필요 (async)
 	//조건2 - await문은 promise반환함수에만 지정가능
 
-	const fetchYoutube = async () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		const pid = process.env.REACT_APP_YOUTUBE_LIST;
-		const num = 6;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-
-		// fetch(baseURL)
-		// 	.then((data) => data.json())
-		// 	.then((json) => {
-		// 		setVids(json.items);
-		// 	});
-		try {
-			const data = await fetch(baseURL);
-			const json = await data.json();
-			setVids(json.items);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
-
 	return (
 		<Layout title={'Youtube'}>
-			{Vids.map((data, idx) => {
+			{Vids?.map(data => {
 				const [date, time] = data.snippet.publishedAt.split('T');
 
 				return (
