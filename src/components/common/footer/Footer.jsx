45948045fdbfaf0508f2612b60229e1fb096c27d
zeeeunee <wldmstv@gmail.com>
2023-12-19 11:43:36 +0900

    리액트에서 외부링크연결방법

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -9,7 +9,10 @@ export default function Footer() {
 			<p>2023 Zeeeunee &Copy; All Rights Reserved.</p>
 			<ul>
 				<li>
-					<FaFacebook />
+					{/* 외부 링크 연결시 일반 a태그 처리 rel=noopener noreferrer 속성 추가해서 window객체에 이전 리액트 컴포넌트의 정보를 참조못하게 처리 */}
+					<a href='https://www.facebook.com' target='_self' rel='noopener noreferrer'>
+						<FaFacebook />
+					</a>
 				</li>
 				<li>
 					<FaTwitter />
