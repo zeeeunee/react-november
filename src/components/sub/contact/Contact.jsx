1cd06721251cbbcf64974b0814c2cc5634e52ad6
zeeeunee <wldmstv@gmail.com>
2023-12-19 13:22:30 +0900

    초기로딩시 로드뷰출력 제외, setCenter안쪽에 roadview함수 호출 제거

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -74,15 +74,19 @@ export default function Contact() {
 		position: mapInfo.current[Index].latlng,
 		image: new kakao.current.maps.MarkerImage(mapInfo.current[Index].imgSrc, mapInfo.current[Index].imgSize, mapInfo.current[Index].imgOpt)
 	});
-	const roadview = useRef(() => {
+
+	const roadview = useCallback(() => {
+		if (!View) return;
 		new kakao.current.maps.RoadviewClient().getNearestPanoId(mapInfo.current[Index].latlng, 50, panoId => {
 			new kakao.current.maps.Roadview(viewFrame.current).setPanoId(panoId, mapInfo.current[Index].latlng);
 		});
-	});
+	}, [Index]);
+
 	const setCenter = useCallback(() => {
 		mapInstance.current.setCenter(mapInfo.current[Index].latlng);
 		roadview.current();
 	}, [Index]);
+
 	//컴포넌트 마운트시 참조객체에 담아놓은 돔 프레임에 지도 인스턴스 출력 및 마커 세팅
 	useEffect(() => {
 		mapFrame.current.innerHTML = '';
@@ -93,7 +97,7 @@ export default function Contact() {
 		marker.current.setMap(mapInstance.current);
 		setTraffic(false);
 		setView(false);
-		roadview.current();
+		roadview();
 		//지도 타입 컨트롤러 추가
 		mapInstance.current.addControl(new kakao.current.maps.MapTypeControl(), kakao.current.maps.ControlPosition.TOPRIGHT);
 		//지도 줌 컨트롤러 추가
@@ -102,13 +106,18 @@ export default function Contact() {
 		mapInstance.current.setZoomable(false);
 		window.addEventListener('resize', setCenter);
 		return () => window.removeEventListener('resize', setCenter);
-	}, [Index]);
+	}, [Index, setCenter]);
 
 	useEffect(() => {
 		Traffic
 			? mapInstance.current.addOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC)
 			: mapInstance.current.removeOverlayMapTypeId(kakao.current.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
+
+	useEffect(() => {
+		viewFrame.current.innerHTML = '';
+		View && roadview();
+	}, [View, roadview]);
 
 	return (
 		<Layout title={'Contact'}>
