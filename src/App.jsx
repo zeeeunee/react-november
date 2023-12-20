1cea2313c4e947bf360646b8e1e0b4ca869927f6
zeeeunee <wldmstv@gmail.com>
2023-12-20 13:18:50 +0900

    flickr데이터전역관리를 위한 slice파일 생성

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchYoutube } from './redux/youtubeSlice';
 import { fetchMember } from './redux/memberSlice';
 import { fetchHistory } from './redux/historySlice';
+import { fetchFlickr } from './redux/flickrSlice';
 
 function App() {
 	const dispatch = useDispatch();
@@ -30,6 +31,7 @@ function App() {
 		dispatch(fetchYoutube());
 		dispatch(fetchMember());
 		dispatch(fetchHistory());
+		dispatch(fetchFlickr());
 	}, [dispatch]);
 
 	return (
