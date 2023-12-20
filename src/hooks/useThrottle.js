c123f8e364f223fd98c3578b51785875ee5feb89
zeeeunee <wldmstv@gmail.com>
2023-12-20 10:43:55 +0900

    useThrottle동작방식주석정리

--- src/hooks/useThrottle.js
+++ src/hooks/useThrottle.js
@@ -5,14 +5,24 @@
 //throttle 적용대표사례 : window event(scroll, resize) 발생시마다 불필요하게 많이 호출되는 함수의 호출횟수를 줄일때
 import { useRef } from 'react';
 
+//setTimeout이 호출되면 delay 뒤에 리턴값 반환이 아니라 호출즉시 return반환
+//setTimeout의 delay값이 끝나기전에 중복호출되면 기존 함수를 무시하고 다시초기화해서 setTimeout이 또 호출됨
+//throttle은 위의 호출을 막기위함
+
 export const useThrottle = (func, gap = 500) => {
+	//초기값을 null상태로 만든다음에 초기 한번은 온전히 setTimeout이 호출되게 처리
 	const eventBlocker = useRef(null);
 
 	return () => {
+		//eventBlocker이 담겨있으면 리턴으로 강제 중지함으서써 setTimeout중복호출하지 않음
 		if (eventBlocker.current) return;
+
+		//setTime이 실햄됨과 동시에 리턴값을 eventBlocker에 담아서 중복호출을 막으면서 gap시간이후에 호출되는 특정 로직을 보장
 		eventBlocker.current = setTimeout(() => {
+			//gap 시간이후에 인수로 전달된 함수를 호출하고
 			func();
 			eventBlocker.current = null;
+			//gap시간이후에 다시 setTimeout을 호출할수 있게됨
 		}, gap);
 	};
 };
