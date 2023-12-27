34544a94f0c1558d7cdfd080ab56efa9ecf1ebdd
zeeeunee <wldmstv@gmail.com>
2023-12-27 13:16:59 +0900

    youtube데이터 react-query로 관리

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -1,67 +1,42 @@
 import Layout from '../../common/layout/Layout';
 import './Youtube.scss';
-import { useState, useEffect } from 'react';
+
 import { useCustomText } from '../../../hooks/useText';
 import { Link } from 'react-router-dom/cjs/react-router-dom.min';
+import { useYoutubeQuery } from '../../../hooks/useYoutubeQuery';
 
 export default function Youtube() {
 	const customText = useCustomText('combined');
 	const shortenText = useCustomText('shorten');
-	const [Vids, setVids] = useState([]);
 
-	//promise then 구문을 async await 변경하기 위한 조건 2가지
-	//조건1 - promise반환 함수를 감싸주는 wrapping함수 필요 (async)
-	//조건2 - await문은 promise반환함수에만 지정가능
-
-	const fetchYoutube = async () => {
-		const api_key = process.env.REACT_APP_YOUTUBE_API;
-		const pid = process.env.REACT_APP_YOUTUBE_LIST;
-		const num = 6;
-		const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-
-		// fetch(baseURL)
-		// 	.then((data) => data.json())
-		// 	.then((json) => {
-		// 		setVids(json.items);
-		// 	});
-		try {
-			const data = await fetch(baseURL);
-			const json = await data.json();
-			setVids(json.items);
-		} catch (err) {
-			console.log(err);
-		}
-	};
-
-	useEffect(() => {
-		fetchYoutube();
-	}, []);
+	const { data: Vids, isSuccess: isYoutube } = useYoutubeQuery();
 
 	return (
 		<Layout title={'Youtube'}>
-			{Vids.map((data, idx) => {
-				const [date, time] = data.snippet.publishedAt.split('T');
+			{isYoutube &&
+				Vids.map(data => {
+					const [date, time] = data.snippet.publishedAt.split('T');
 
-				return (
-					<article key={data.id}>
-						<h2>{shortenText(data.snippet.title, 50)}</h2>
+					return (
+						<article key={data.id}>
+							<h2>{shortenText(data.snippet.title, 50)}</h2>
 
-						<div className='txt'>
-							<p>{shortenText(data.snippet.description, 250)}</p>
-							<div className='infoBox'>
-								<span>{customText(date, '.')}</span>
-								<em>{time.split('Z')[0]}</em>
+							<div className='txt'>
+								<p>{shortenText(data.snippet.description, 250)}</p>
+								<div className='infoBox'>
+									<span>{customText(date, '.')}</span>
+									<em>{time.split('Z')[0]}</em>
+								</div>
 							</div>
-						</div>
 
-						<div className='pic'>
-							<Link to={`/detail/${data.id}`}>
-								<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
-							</Link>
-						</div>
-					</article>
-				);
-			})}
+							<div className='pic'>
+								<Link to={`/detail/${data.id}`}>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</Link>
+							</div>
+						</article>
+					);
+				})}
 		</Layout>
 	);
 }
---
+++ src/hooks/useYoutubeQuery.js
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+
+const fetchYoutube = async () => {
+	const api_key = process.env.REACT_APP_YOUTUBE_API;
+	const pid = process.env.REACT_APP_YOUTUBE_LIST;
+	const num = 6;
+	const baseURL = `https://www.googleapis.com/youtube/v3/playlistItems?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
+
+	const response = await fetch(baseURL);
+	const data = await response.json();
+	return data.items;
+};
+
+export const useYoutubeQuery = () => {
+	return useQuery(['fetchYoutube'], fetchYoutube, {
+		refetchOnMount: false,
+		refetchOnWindowFocus: false,
+		cacheTime: 1000 * 60 * 60 * 24,
+		staleTime: 100 * 60 * 60 * 24,
+		retry: 3 //만약 데이터 요청 실패시 재시도 횟수, default:3
+	});
+};
