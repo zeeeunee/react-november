1832d9fba3fdb8c70857ec40ef143062b771c9be
zeeeunee <wldmstv@gmail.com>
2023-12-29 17:23:28 +0900

    swiper 옵션 객체형태로 정리

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -5,31 +5,26 @@ import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import { useCustomText } from '../../../hooks/useText';
 
 //Visual parent component
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
 	const shortenText = useCustomText('shorten');
-
 	const swiperRef = useRef(null);
 
-	//swiper pagination option
-	const pagination = useRef({
-		clickable: true,
-		renderBullet: (index, className) => `<span class=${className}>${index + 1}</span>`
-	});
-
-	//swiper autoplay option
-	const autoplay = useRef({
-		delay: 2000,
-		disableOnInteraction: true
+	const swiperOption = useRef({
+		modules: [Pagination, Autoplay],
+		pagination: { clickable: true, renderBullet: (index, className) => `<span class=${className}>${index + 1}</span>` },
+		autoplay: { delay: 2000, disableOnInteraction: true },
+		loop: true
 	});
 
 	return (
 		<figure className='Visual'>
-			<Swiper modules={[Pagination, Autoplay]} pagination={pagination.current} autoplay={autoplay.current} loop={true}>
+			{/* <Swiper modules={[Pagination, Autoplay]} pagination={pagination.current} autoplay={autoplay.current} loop={true}> */}
+			<Swiper {...swiperOption.current}>
 				{youtube.map((vid, idx) => {
 					if (idx >= 5) return null;
 
@@ -47,8 +42,7 @@ export default function Visual() {
 								<div className='txtBox'>
 									<h2>{shortenText(vid.snippet.title, 50)}</h2>
 
-									<Link to={`/detail/${vid.id}`} onMouseEnter={swiperRef.current.autoplay.stop} onMouseLeave={swiperRef.current.autoplay.start}>
-										{/*마우스올리면 스와이프안되게하는기능 */}
+									<Link to={`/detail/${vid.id}`} onMouseEnter={swiperRef.current?.autoplay?.stop} onMouseLeave={swiperRef.current?.autoplay?.start}>
 										<span></span>View Detail
 									</Link>
 								</div>
@@ -56,21 +50,14 @@ export default function Visual() {
 						</SwiperSlide>
 					);
 				})}
-
 				<Btns swiperRef={swiperRef} />
 			</Swiper>
 		</figure>
 	);
 }
-
 //Swiper control child component
 function Btns({ swiperRef }) {
 	swiperRef.current = useSwiper();
-
-	useEffect(() => {
-		swiperRef.current.init(0);
-		swiperRef.current.slideNext(300);
-	}, [swiperRef]);
 
 	return (
 		<nav className='swiperController'>
