7185936d7de660afea2a85bff047c01439e03e25
zeeeunee <wldmstv@gmail.com>
2023-12-29 12:23:14 +0900

    처음 마운트시 마지막 패널이 보이는 이슈해결하기 위해서 강제로 처음에만 다음슬라이드 넘기기 (loop사용시)

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -4,11 +4,17 @@ import 'swiper/css/pagination';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 function Btns() {
 	//Swiper컴포넌트 안쪽에 있는 또다른 자식 컴포넌트 안쪽에서만 useSwiper hook사용가능
 	//hook으로부터 생성된 객체(인스턴스)에는 다양한 prototype메서드와 property값 활용가능
 	const swiper = useSwiper();
+
+	useEffect(() => {
+		swiper.slideNext(300);
+	}, [swiper]); //강제로 처음패널로 보이게 만들기
+
 	return (
 		<nav className='swiperController'>
 			<button
@@ -26,7 +32,6 @@ function Btns() {
 
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
-
 	return (
 		<figure className='Visual'>
 			<Swiper
@@ -47,12 +52,13 @@ export default function Visual() {
 					return (
 						<SwiperSlide key={vid.id}>
 							<div className='inner'>
-								<h3>{vid.snippet.title}</h3>
+								<h3>
+									{idx + 1} {vid.snippet.title}
+								</h3>
 							</div>
 						</SwiperSlide>
 					);
 				})}
-
 				<Btns />
 			</Swiper>
 		</figure>
