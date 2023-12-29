adbfcfbc36b5a0997669dea86369bc46d9efef1f
zeeeunee <wldmstv@gmail.com>
2023-12-29 16:28:56 +0900

    자식컴포넌트에 만들어진 swiper인스턴스 객체를 부모의 빈참조객체에 전달해서 부모컴포넌트에서 자식컴포넌트의 autoplay 메서드 사용

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -5,54 +5,34 @@ import { Link } from 'react-router-dom';
 import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper';
 import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useCustomText } from '../../../hooks/useText';
 
-function Btns() {
-	//Swiper컴포넌트 안쪽에 있는 또다른 자식 컴포넌트 안쪽에서만 useSwiper hook사용가능
-	//hook으로부터 생성된 객체(인스턴스)에는 다양한 prototype메서드와 property값 활용가능
-	const swiper = useSwiper();
-
-	useEffect(() => {
-		swiper.init(0);
-		swiper.slideNext(300);
-	}, [swiper]); //강제로 처음패널로 보이게 만들기
-
-	return (
-		<nav className='swiperController'>
-			<button
-				onClick={() => {
-					//다시 롤링시작 버튼 클릭시 delay바로 slide넘기기위해서 일단은 다음 슬라이드 넘기고 동시에 롤링 재시작
-					swiper.slideNext(300);
-					swiper.autoplay.start();
-				}}>
-				start
-			</button>
-			<button onClick={() => swiper.autoplay.stop()}>stop</button>
-		</nav>
-	);
-}
-
+//Visual parent component
 export default function Visual() {
 	const { youtube } = useSelector(store => store.youtubeReducer);
 	const shortenText = useCustomText('shorten');
+
+	const swiperRef = useRef(null);
+
+	//swiper pagination option
+	const pagination = useRef({
+		clickable: true,
+		renderBullet: (index, className) => `<span class=${className}>${index + 1}</span>`
+	});
+
+	//swiper autoplay option
+	const autoplay = useRef({
+		delay: 2000,
+		disableOnInteraction: true
+	});
+
 	return (
 		<figure className='Visual'>
-			<Swiper
-				modules={[Pagination, Autoplay]}
-				pagination={{
-					clickable: true,
-					renderBullet: (index, className) => {
-						return `<span class=${className}>${index + 1}</span>`;
-					}
-				}}
-				autoplay={{
-					delay: 4000,
-					disableOnInteraction: true
-				}}
-				loop={true}>
-				{youtube?.map((vid, idx) => {
+			<Swiper modules={[Pagination, Autoplay]} pagination={pagination.current} autoplay={autoplay.current} loop={true}>
+				{youtube.map((vid, idx) => {
 					if (idx >= 5) return null;
+
 					return (
 						<SwiperSlide key={vid.id}>
 							<div className='inner'>
@@ -66,7 +46,9 @@ export default function Visual() {
 								</div>
 								<div className='txtBox'>
 									<h2>{shortenText(vid.snippet.title, 50)}</h2>
-									<Link to={`/detail/${vid.id}`}>
+
+									<Link to={`/detail/${vid.id}`} onMouseEnter={swiperRef.current.autoplay.stop} onMouseLeave={swiperRef.current.autoplay.start}>
+										{/*마우스올리면 스와이프안되게하는기능 */}
 										<span></span>View Detail
 									</Link>
 								</div>
@@ -74,14 +56,32 @@ export default function Visual() {
 						</SwiperSlide>
 					);
 				})}
-				<Btns />
+
+				<Btns swiperRef={swiperRef} />
 			</Swiper>
 		</figure>
 	);
 }
 
-/*
-	React에서 Swiper의 코어기능을 적용하기 위해서는 useSwiper라는 hook호출
-	Swiper안쪽에서 또다른 컴포넌트를 연결해주고 그 안쪽에서 useSwiper로 부터 객체생성
-	해당 자식 컴포넌트 안쪽에서 생성된 객체로부터 swiper core에 등록되어 있는 모든 메서드, 프로퍼티를 리액트에서도 사용가능
-*/
+//Swiper control child component
+function Btns({ swiperRef }) {
+	swiperRef.current = useSwiper();
+
+	useEffect(() => {
+		swiperRef.current.init(0);
+		swiperRef.current.slideNext(300);
+	}, [swiperRef]);
+
+	return (
+		<nav className='swiperController'>
+			<button
+				onClick={() => {
+					swiperRef.current.slideNext(300);
+					swiperRef.current.autoplay.start();
+				}}>
+				start
+			</button>
+			<button onClick={() => swiperRef.current.autoplay.stop()}>stop</button>
+		</nav>
+	);
+}
