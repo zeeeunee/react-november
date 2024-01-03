ffde2b316a58cce11637e328e8285ad570253a53
zeeeunee <wldmstv@gmail.com>
2024-01-03 13:19:28 +0900

    breakpoinsts로 디바이스폭에 따른 swiper옵션 변경으로 반응형 처리

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -9,10 +9,13 @@ export default function Visual() {
 	const Vids = useSelector(store => store.youtube.data);
 	const swiperOpt = useRef({
 		modules: [Autoplay],
-		autoplay: { delay: 4000, disableOnInteraction: true },
+		autoplay: { delay: 2000, disableOnInteraction: true },
 		loop: true,
-		slidesPerView: 3,
+		slidesPerView: 1,
 		centeredSlides: true, //가운데 배치된 패널이 활성화
+		breakpoints: {
+			1000: { slidesPerView: 3 }
+		},
 		onSwiper: swiper => {
 			swiper.slideNext(300); //처음 롤링했을때 왼쪽 빈 패널이 없게 한번 롤링시켜줌
 		}
@@ -27,7 +30,12 @@ export default function Visual() {
 						<SwiperSlide key={data.id}>
 							<div className='inner'>
 								<div className='picBox'>
-									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+									<p>
+										<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+									</p>
+									<p>
+										<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+									</p>
 								</div>
 								<div className='txtBox'>
 									<h2>{data.snippet.title}</h2>
