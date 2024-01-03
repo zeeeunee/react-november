4af68cf7cd101a4a19d3170a2718cfc72ca97f16
zeeeunee <wldmstv@gmail.com>
2024-01-03 15:55:13 +0900

    swiper의 활성화순번 state에 옮겨담기

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -6,12 +6,15 @@ import { useRef, useState } from 'react';
 
 export default function Visual() {
 	const { isSuccess, data } = useYoutubeQuery();
+	const [Index, setIndex] = useState(0);
+
 	const swiperOpt = useRef({
 		loop: true,
 		slidesPerView: 1,
 		spaceBetween: 50,
 		centeredSlides: true,
 		onSwiper: swiper => swiper.slideNext(300),
+		onSlideChange: swiper => setIndex(swiper.realIndlex),
 		breakpoints: {
 			1000: { slidesPerView: 2 },
 			1400: { slidesPerView: 3 }
