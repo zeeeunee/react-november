195e6d362b360194b18aff78e90927f29c3d45c5
zeeeunee <wldmstv@gmail.com>
2024-01-03 12:27:25 +0900

    스타일추가

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -2,18 +2,20 @@ import { useRef } from 'react';
 import './Visual.scss';
 import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 export default function Visual() {
 	const Vids = useSelector(store => store.youtube.data);
 	const swiperOpt = useRef({
 		spaceBetween: 50,
 		loop: true,
-		slidesPerView: 3
+		slidesPerView: 3,
+		centeredSlides: true
 	});
 
 	return (
 		<figure className='Visual'>
-			<Swiper {...swiperOpt}>
+			<Swiper {...swiperOpt.current}>
 				{Vids.map((data, idx) => {
 					if (idx >= 5) return null;
 					return (
