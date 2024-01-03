676cc1c6fb5b142d73264787fe0f1fd5216f5541
zeeeunee <wldmstv@gmail.com>
2024-01-03 12:04:01 +0900

    youtube데이터 메인에 가져오기

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,5 +1,26 @@
 import './Visual.scss';
+import { useSelector } from 'react-redux';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function Visual() {
-	return <figure className='Visual'></figure>;
+	const Vids = useSelector(store => store.youtube.data);
+
+	return (
+		<figure className='Visual'>
+			<Swiper>
+				{Vids.map((data, idx) => {
+					if (idx >= 5) return null;
+					return (
+						<SwiperSlide key={data.id}>
+							<div className='inner'>
+								<div className='picBox'>
+									<img src={data.snippet.thumbnails.standard.url} alt={data.snippet.title} />
+								</div>
+							</div>
+						</SwiperSlide>
+					);
+				})}
+			</Swiper>
+		</figure>
+	);
 }
