4c7e03dfb41fc77a193cbb798d25f74128b4a55b
zeeeunee <wldmstv@gmail.com>
2024-01-04 11:56:54 +0900

    스크롤시 버튼 자동 활성화

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -7,25 +7,53 @@ import './Btns.scss';
 
 export default function Btns() {
 	const [Index, setIndex] = useState(0);
-	const num = useRef(4);
-	const btns = useRef(null);
+	const [Num, setNum] = useState(0);
 	const secs = useRef(null);
 	const wrap = useRef(null);
+	const btns = useRef(null);
+
+	const activation = () => {
+		const scroll = wrap.current.scrollTop;
+
+		secs.current.forEach((sec, idx) => {
+			if (scroll >= secs.current[idx].offsetTop) {
+				Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+				btns.current.children[idx].classList.add('on');
+			}
+		});
+		/*
+		if (scroll >= secs.current[0].offsetTop) {
+			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+			btns.current.children[0].classList.add('on');
+		}
+		if (scroll >= secs.current[1].offsetTop) {
+			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+			btns.current.children[1].classList.add('on');
+		}
+		if (scroll >= secs.current[2].offsetTop) {
+			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+			btns.current.children[2].classList.add('on');
+		}
+		if (scroll >= secs.current[3].offsetTop) {
+			Array.from(btns.current.children).forEach(btn => btn.classList.remove('on'));
+			btns.current.children[3].classList.add('on');
+		}
+    */
+	};
+
 	useEffect(() => {
 		wrap.current = document.querySelector('.wrap');
 		secs.current = document.querySelectorAll('.myScroll');
+		setNum(secs.current.length);
 
-		wrap.current.addEventListener('scroll', e => {
-			console.log('scroll', e.target.scrollTop);
-			console.log('offset', secs.current[1].offsetTop);
-		});
+		wrap.current.addEventListener('scroll', activation);
 	}, []);
 	return (
-		<ul className='Btns'>
-			{Array(num.current)
+		<ul className='Btns' ref={btns}>
+			{Array(Num)
 				.fill()
 				.map((_, idx) => {
-					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
+					return <li key={idx} className={idx === Index ? 'on' : ''} onClick={() => setIndex(idx)}></li>;
 				})}
 		</ul>
 	);
