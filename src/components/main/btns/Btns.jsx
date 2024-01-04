93687f076a11f68faece01d5b7f0c6996df479ec
zeeeunee <wldmstv@gmail.com>
2024-01-04 11:17:38 +0900

    Btns 스타일링

--- src/components/main/btns/Btns.jsx
+++ src/components/main/btns/Btns.jsx
@@ -1,5 +1,20 @@
+import { useEffect, useRef, useState } from 'react';
 import './Btns.scss';
 
 export default function Btns() {
-	return <ul className='Btns'>Btns</ul>;
+	const [Index, setIndex] = useState(0);
+	const num = useRef(4);
+
+	useEffect(() => {
+		num.current = document.body.querySelectorAll('.myScroll').length;
+	}, []);
+	return (
+		<ul className='Btns'>
+			{Array(num.current)
+				.fill()
+				.map((_, idx) => {
+					return <li key={idx} className={idx === Index ? 'on' : ''}></li>;
+				})}
+		</ul>
+	);
 }
