7068b26d3a68dcb9c49be1c35b1eb1c7b5161503
zeeeunee <wldmstv@gmail.com>
2024-01-05 18:17:53 +0900

    state에 따른 화면 재랜더링

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,5 +1,25 @@
 import './Banner.scss';
+import { useScroll } from '../../../hooks/useScroll';
+import { useEffect, useRef, useState } from 'react';
 
 export default function Banner() {
-	return <section className='Banner myScroll'>Banner</section>;
+	const [Frame, setFrame] = useState(null);
+	const refBanner = useRef(null);
+	const [Scrolled, setScrolled] = useState(0);
+
+	const { getCurrentScroll } = useScroll(Frame);
+
+	useEffect(() => {
+		setFrame(refBanner.current?.closest('.wrap'));
+		Frame?.addEventListener('scroll', () => {
+			const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+			setScrolled(scroll);
+		});
+	}, [Frame, getCurrentScroll]);
+
+	return (
+		<section className='Banner myScroll' ref={refBanner}>
+			<div className='box' style={{ transform: `rotate(${Scrolled / 2}deg) scale(${1 + Scrolled / 400})`, opacity: 1 - Scrolled / 400 }}></div>
+		</section>
+	);
 }
