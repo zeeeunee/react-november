8dc89b8a6f00a6cb01d0fe390255179ce4278dff
zeeeunee <wldmstv@gmail.com>
2024-01-05 16:58:47 +0900

    커스텀훅 자체적으로 baseLine값 적용해서 scroll값 반환

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -8,22 +8,17 @@ export default function Pics() {
 	const boxEl = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
 
-	//hook으로부터 현재 보정된 scroll값을 반환받아서
-	//해당 값을 참조객체의 style값으로 연동
-	//scroll값이 바뀔때마다 불필요하게 재랜더링하지 않으면서 스크롤값 스타일에 연동
 	const handleScroll = useCallback(() => {
 		const scroll = getCurrentScroll(thisEl.current);
+
 		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
 	}, [getCurrentScroll]);
 
-	//컴포넌트 마운트시 wrap요소를 Frame State에담음
-	//State에 담겨있는 wrap요소는 최신 scrollTop을 가지고 있는 동적인 상태
 	useEffect(() => {
 		setFrame(thisEl.current?.closest('.wrap'));
 	}, []);
 
 	useEffect(() => {
-		//Frame에 scroll이벤트 연결
 		Frame?.addEventListener('scroll', handleScroll);
 		return () => Frame?.removeEventListener('scroll', handleScroll);
 	}, [Frame, handleScroll]);
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,15 +1,12 @@
 import Anime from '../asset/anime';
 
 export function useScroll(scrollFrame) {
-	//기존에 scrollTop값을 제어하는 wrap요소를 참조객체에 담아서 반환하는것이 문제
-	//이유: wrap.scrollTop의 변경되는 값을 계속 활용해되는데
-	//참조객체에 담으면 담는 순간의 참조객체값이 고정되는 문제 발생
-	//해결방법: wrap요소를 호출 부모컴포넌트에서 State에 담도록 처리
 	const scrollTo = targetPos => {
 		new Anime(scrollFrame.current, { scroll: targetPos });
 	};
-	const getCurrentScroll = selfEl => {
-		const scroll = scrollFrame.scrollTop;
+
+	const getCurrentScroll = (selfEl, baseLine = -window.innerHeight / 2) => {
+		const scroll = scrollFrame.scrollTop - baseLine;
 		const modifiedScroll = scroll - selfEl?.offsetTop;
 		return modifiedScroll;
 	};
