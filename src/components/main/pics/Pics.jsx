13809fc66cd2d6dd39b70c99ff4e0dd55171b894
zeeeunee <wldmstv@gmail.com>
2024-01-05 17:53:30 +0900

    scroll값이용해서 직접 style값 제어

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -5,24 +5,38 @@ import './Pics.scss';
 export default function Pics() {
 	const [Frame, setFrame] = useState(null);
 	const thisEl = useRef(null);
-	const boxEl = useRef(null);
+	const titEl = useRef(null);
+	const titEl2 = useRef(null);
 	const { getCurrentScroll } = useScroll(Frame);
 
 	const handleScroll = useCallback(() => {
 		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
 
-		scroll >= 0 && (boxEl.current.style.transform = `translateX(${scroll}px)`);
+		if (scroll >= 0) {
+			titEl.current.style.transform = `translateX(${scroll}px)`;
+			titEl.current.style.opacity = 1 - scroll / 800;
+			titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
+			titEl2.current.style.opacity = 1 - scroll / 500;
+		}
 	}, [getCurrentScroll]);
+
 	useEffect(() => {
 		setFrame(thisEl.current?.closest('.wrap'));
 	}, []);
+
 	useEffect(() => {
 		Frame?.addEventListener('scroll', handleScroll);
 		return () => Frame?.removeEventListener('scroll', handleScroll);
 	}, [Frame, handleScroll]);
+
 	return (
 		<section className='Pics myScroll' ref={thisEl}>
-			<div className='box' ref={boxEl}></div>
+			<h3 className='tit' ref={titEl}>
+				FLICKR
+			</h3>
+			<h4 className='tit2' ref={titEl2}>
+				PREVIEW
+			</h4>
 		</section>
 	);
 }
