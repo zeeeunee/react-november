e7ad41811103979a08445eedcfecb8d58e486a55
zeeeunee <wldmstv@gmail.com>
2024-01-08 11:12:04 +0900

    바뀐 useScroll구문에 맞게 Pics컴포넌트 수정

--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,41 +1,27 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
 
 export default function Pics() {
-	const [Frame, setFrame] = useState(null);
-	const thisEl = useRef(null);
 	const titEl = useRef(null);
 	const titEl2 = useRef(null);
-	const { getCurrentScroll } = useScroll(Frame);
 
-	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(thisEl.current, -window.innerHeight / 2);
+	const handleCustomScroll = scroll => {
+		titEl.current.style.transform = `translateX(${scroll}px)`;
+		titEl.current.style.opacity = 1 - scroll / 800;
+		titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
+		titEl2.current.style.opacity = 1 - scroll / 500;
+	};
 
-		if (scroll >= 0) {
-			titEl.current.style.transform = `translateX(${scroll}px)`;
-			titEl.current.style.opacity = 1 - scroll / 800;
-			titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
-			titEl2.current.style.opacity = 1 - scroll / 500;
-		}
-	}, [getCurrentScroll]);
-
-	useEffect(() => {
-		setFrame(thisEl.current?.closest('.wrap'));
-	}, []);
-
-	useEffect(() => {
-		Frame?.addEventListener('scroll', handleScroll);
-		return () => Frame?.removeEventListener('scroll', handleScroll);
-	}, [Frame, handleScroll]);
+	const { refEl } = useScroll(handleCustomScroll);
 
 	return (
-		<section className='Pics myScroll' ref={thisEl}>
+		<section className='Pics myScroll' ref={refEl}>
 			<h3 className='tit' ref={titEl}>
 				FLICKR
 			</h3>
 			<h4 className='tit2' ref={titEl2}>
-				PREVIEW
+				PREIVIEW
 			</h4>
 		</section>
 	);
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,7 +1,6 @@
 import Anime from '../asset/anime';
 import { useState, useEffect, useRef, useCallback } from 'react';
 
-//순서3 - 부모컴포넌트로 커스텀스크롤 함수를 파라미터를 통해 내부로 전달
 export function useScroll(customHandler) {
 	const refEl = useRef(null);
 	const [Frame, setFrame] = useState(null);
@@ -19,8 +18,6 @@ export function useScroll(customHandler) {
 		[Frame]
 	);
 
-	//순서4- 전달받은 커스텀스크롤 함수를 내부에 있는 handleScroll함수 안쪽에 호출에서
-	//내부적으로 getCurrentScroll값이 반환하고 있는 스크롤값과 연동시켜줌
 	const handleScroll = useCallback(() => {
 		const scroll = getCurrentScroll();
 		if (scroll >= 0) customHandler(scroll);
@@ -30,11 +27,9 @@ export function useScroll(customHandler) {
 		setFrame(document.querySelector('.wrap'));
 	}, []);
 
-	//순서5- 해당 커스텀훅을 호출하고 있는 부모 컴포넌트가 마운트시 handleScroll함수에 scoll이벤트 연결
 	useEffect(() => {
 		Frame?.addEventListener('scroll', handleScroll);
 		return () => Frame?.removeEventListener('scroll', handleScroll);
 	}, [Frame, handleScroll]);
-
 	return { scrollTo, getCurrentScroll, Frame, refEl };
 }
