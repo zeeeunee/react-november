b1d4620af5a53a97cba511c36aee9b4b6c0e5c8a
zeeeunee <wldmstv@gmail.com>
2024-01-08 10:06:13 +0900

    Banner컴포넌트를 Pics와 마찬가지로 핸드러함수 안쪽에서 scroll값 연동처리

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,25 +1,34 @@
 import './Banner.scss';
 import { useScroll } from '../../../hooks/useScroll';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 export default function Banner() {
 	const [Frame, setFrame] = useState(null);
 	const refBanner = useRef(null);
-	const [Scrolled, setScrolled] = useState(0);
+	const boxEl = useRef(null);
 
 	const { getCurrentScroll } = useScroll(Frame);
 
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
+		if (scroll >= 0) {
+			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+			boxEl.current.style.opacity = 1 - scroll / 400;
+		}
+	}, [getCurrentScroll]);
+
 	useEffect(() => {
 		setFrame(refBanner.current?.closest('.wrap'));
-		Frame?.addEventListener('scroll', () => {
-			const scroll = getCurrentScroll(refBanner.current, -window.innerHeight / 2);
-			setScrolled(scroll);
-		});
-	}, [Frame, getCurrentScroll]);
+	}, []);
+
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
 
 	return (
 		<section className='Banner myScroll' ref={refBanner}>
-			<div className='box' style={{ transform: `rotate(${Scrolled / 2}deg) scale(${1 + Scrolled / 400})`, opacity: 1 - Scrolled / 400 }}></div>
+			<div className='box' ref={boxEl}></div>
 		</section>
 	);
 }
