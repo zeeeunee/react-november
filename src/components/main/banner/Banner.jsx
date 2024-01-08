9ba6bb71c094b13158cbd93d599dbcbe0bcc3833
zeeeunee <wldmstv@gmail.com>
2024-01-08 12:02:27 +0900

    scroll연동영역을 벗어나면 원래수치값으로 강제 초기화

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -3,16 +3,17 @@ import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
 export default function Banner() {
 	const boxEl = useRef(null);
-
-	//순서1 - useScroll훅으로 전달한 해당 컴포넌트에서의 커스텀스크롤 함수 정의
 	const customHandleScroll = scroll => {
-		boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
-		boxEl.current.style.opacity = 1 - scroll / 400;
+		if (scroll >= 0) {
+			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+			boxEl.current.style.opacity = 1 - scroll / 400;
+		} else {
+			boxEl.current.style.transform = `rotate(0deg) scale(1)`;
+			boxEl.current.style.opacity = 1;
+		}
 	};
 
-	//순서2- 커스텀스크롤 함수를 useScroll호출시 인수로 전달
 	const { refEl } = useScroll(customHandleScroll);
-
 	return (
 		<section className='Banner myScroll' ref={refEl}>
 			<div className='box' ref={boxEl}></div>
--- src/components/main/pics/Pics.jsx
+++ src/components/main/pics/Pics.jsx
@@ -1,7 +1,6 @@
 import { useRef } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import './Pics.scss';
-
 export default function Pics() {
 	const titEl = useRef(null);
 	const titEl2 = useRef(null);
@@ -12,11 +11,15 @@ export default function Pics() {
 			titEl.current.style.opacity = 1 - scroll / 800;
 			titEl2.current.style.transform = ` scale(${1 + scroll / 400}) translateX(${scroll}px)`;
 			titEl2.current.style.opacity = 1 - scroll / 500;
+		} else {
+			titEl.current.style.transform = `translateX(0px)`;
+			titEl.current.style.opacity = 1;
+			titEl2.current.style.transform = ` scale(1) translateX(0px)`;
+			titEl2.current.style.opacity = 1;
 		}
 	};
 
 	const { refEl } = useScroll(handleCustomScroll);
-
 	return (
 		<section className='Pics myScroll' ref={refEl}>
 			<h3 className='tit' ref={titEl}>
