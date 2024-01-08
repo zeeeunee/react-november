bed6619a33653e980949b47a30ff33a4ea87d122
zeeeunee <wldmstv@gmail.com>
2024-01-08 11:08:33 +0900

    커스텀스크롤 함수를 useScoll훅으로 전달처리

--- src/components/main/banner/Banner.jsx
+++ src/components/main/banner/Banner.jsx
@@ -1,27 +1,19 @@
-import { useEffect, useRef, useCallback } from 'react';
+import { useRef } from 'react';
 import { useScroll } from '../../../hooks/useScroll';
 import './Banner.scss';
-
 export default function Banner() {
 	const boxEl = useRef(null);
 
-	//순서3 - 커스텀훅 호출시 useScroll이 제공하고 있는 빈참조객체 가져옴
-	const { getCurrentScroll, Frame, refEl } = useScroll();
+	//순서1 - useScroll훅으로 전달한 해당 컴포넌트에서의 커스텀스크롤 함수 정의
+	const customHandleScroll = scroll => {
+		boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
+		boxEl.current.style.opacity = 1 - scroll / 400;
+	};
 
-	const handleScroll = useCallback(() => {
-		const scroll = getCurrentScroll(-window.innerHeight / 2);
-		if (scroll >= 0) {
-			boxEl.current.style.transform = `rotate(${scroll / 2}deg) scale(${1 + scroll / 400})`;
-			boxEl.current.style.opacity = 1 - scroll / 400;
-		}
-	}, [getCurrentScroll]);
-	useEffect(() => {
-		Frame?.addEventListener('scroll', handleScroll);
-		return () => Frame?.removeEventListener('scroll', handleScroll);
-	}, [Frame, handleScroll]);
+	//순서2- 커스텀스크롤 함수를 useScroll호출시 인수로 전달
+	const { refEl } = useScroll(customHandleScroll);
 
 	return (
-		//순서4- 원하는 요소에 빈 참조객체 연결
 		<section className='Banner myScroll' ref={refEl}>
 			<div className='box' ref={boxEl}></div>
 		</section>
--- src/hooks/useScroll.js
+++ src/hooks/useScroll.js
@@ -1,25 +1,40 @@
 import Anime from '../asset/anime';
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useCallback } from 'react';
 
-export function useScroll() {
-	//순서1 - 커스텀훅 안쪽에서 자체적으로 빈 참조객체 생성
+//순서3 - 부모컴포넌트로 커스텀스크롤 함수를 파라미터를 통해 내부로 전달
+export function useScroll(customHandler) {
 	const refEl = useRef(null);
 	const [Frame, setFrame] = useState(null);
+
 	const scrollTo = targetPos => {
 		Frame && new Anime(Frame, { scroll: targetPos });
 	};
 
-	const getCurrentScroll = (baseLine = 0) => {
-		const scroll = Frame.scrollTop - baseLine;
-		//순서5- 부모컴포넌트에서 참조객체 연결된 값을 hook내부적으로 활용
-		const modifiedScroll = scroll - refEl.current?.offsetTop;
-		return modifiedScroll;
-	};
+	const getCurrentScroll = useCallback(
+		(baseLine = -window.innerHeight / 2) => {
+			const scroll = Frame.scrollTop - baseLine;
+			const modifiedScroll = scroll - refEl.current?.offsetTop;
+			return modifiedScroll;
+		},
+		[Frame]
+	);
+
+	//순서4- 전달받은 커스텀스크롤 함수를 내부에 있는 handleScroll함수 안쪽에 호출에서
+	//내부적으로 getCurrentScroll값이 반환하고 있는 스크롤값과 연동시켜줌
+	const handleScroll = useCallback(() => {
+		const scroll = getCurrentScroll();
+		if (scroll >= 0) customHandler(scroll);
+	}, [getCurrentScroll, customHandler]);
 
 	useEffect(() => {
 		setFrame(document.querySelector('.wrap'));
 	}, []);
 
-	//순서2 - 부모에서 해당 참조객체를 활용하도록 리턴
+	//순서5- 해당 커스텀훅을 호출하고 있는 부모 컴포넌트가 마운트시 handleScroll함수에 scoll이벤트 연결
+	useEffect(() => {
+		Frame?.addEventListener('scroll', handleScroll);
+		return () => Frame?.removeEventListener('scroll', handleScroll);
+	}, [Frame, handleScroll]);
+
 	return { scrollTo, getCurrentScroll, Frame, refEl };
 }
