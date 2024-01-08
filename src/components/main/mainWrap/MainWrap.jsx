c1585c41cf016f2b4d06395446736282a4c0a2c2
zeeeunee <wldmstv@gmail.com>
2024-01-08 12:19:03 +0900

    svg출력

--- src/components/main/mainWrap/MainWrap.jsx
+++ src/components/main/mainWrap/MainWrap.jsx
@@ -1,5 +1,6 @@
 import Banner from '../banner/Banner';
 import Btns from '../btns/Btns';
+import Illust from '../illust/Illust';
 import Info from '../info/Info';
 import Pics from '../pics/Pics';
 import Visual from '../visual/Visual';
@@ -11,6 +12,7 @@ export default function MainWrap() {
 			<Visual />
 			<Info />
 			<Pics />
+			<Illust />
 			<Banner />
 			<Btns />
 			{/* <Btns frame={스크롤제어할 프레임요소 클래스명} items={스크롤이 걸릴 영역 공통클래스} base={활성화기준점} isAuto={autoScrolling유무(boolean : default-false)} /> */}
